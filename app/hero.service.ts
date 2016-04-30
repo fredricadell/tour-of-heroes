3d178841cf6f81161a1f6c8484e72bbd09bdf506
fredricadell <fredric@adell.me>
2016-04-30 07:43:29 +0200

    Uppdaterad app.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,9 +1,15 @@
 import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
+import {Hero} from './hero';
 
 @Injectable()
 export class HeroService {
     getHeroes(){
         return Promise.resolve(HEROES);
+    }
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+        );
     }
 }
